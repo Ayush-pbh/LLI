6b74d57559334c1710d28601be515c328a30c745
Ayush Tripathi <yush.pbh@gmail.com>
2022-12-15 19:21:19 +0530

    Testing for page rendering and static file visibilty

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+app.use(express.static('site'))
 const path = require('path');
 const router = express.Router();
 
 
+
 router.get('/',function(req,res){
-  res.status(200).json({message:"Hello Its Working!"})
+  // res.status(200).json({message:"Hello Its Working!"})
+  res.sendFile(path.join(__dirname+'/site/index.html'))
+});
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/site/about.html'))
 });
 
 router.get('/bye',function(req,res){
@@ -13,7 +19,11 @@ router.get('/bye',function(req,res){
 });
 
 //add the router
+
 app.use('/', router);
+
+
 app.listen(process.env.port || 8080);
+
 
 console.log('Running at Port 8080');
