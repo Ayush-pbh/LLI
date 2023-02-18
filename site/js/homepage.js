6c991f99a4a0c88987faa3382a8b4987133f0988
Ayush Tripathi <yush.pbh@gmail.com>
2023-02-19 04:52:39 +0530

    Fixed Torch

--- site/js/homepage.js
+++ site/js/homepage.js
@@ -214,3 +214,70 @@ window.onload = function () {
     // Socket
     initSocket()
 }
+
+
+
+// TORCH
+let torchInterval = undefined;
+let flagg = true
+// //Test browser support
+const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
+
+if (SUPPORTS_MEDIA_DEVICES) {
+  //Get the environment camera (usually the second one)
+  navigator.mediaDevices.enumerateDevices().then(devices => {
+  
+    const cameras = devices.filter((device) => device.kind === 'videoinput');
+
+    if (cameras.length === 0) {
+        throw 'No camera found on this device.';
+    }
+    const camera = cameras[cameras.length - 1];
+
+    // Create stream and get video track
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        deviceId: camera.deviceId,
+        facingMode: ['user', 'environment'],
+        height: {ideal: 1080},
+        width: {ideal: 1920}
+      }
+    }).then(stream => {
+      const track = stream.getVideoTracks()[0];
+
+      //Create image capture object and get camera capabilities
+      const imageCapture = new ImageCapture(track)
+      const photoCapabilities = imageCapture.getPhotoCapabilities().then(() => {
+
+        //todo: check if camera has a torch
+
+        //let there be light!
+        const btn = document.querySelector('.switch');
+        btn.addEventListener('click', function(){
+            torchInterval = setInterval(() => {
+                track.applyConstraints({
+                    advanced: [{torch: flagg}]
+                });
+                flagg = !flagg
+            }, 1000);
+        //   Vibrate in SOS mode....
+          navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100,100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100,100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100,100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]);
+          
+        });
+      });
+    });
+  });
+  
+}
+
+
+function ddf(){
+    const btn = document.querySelector('.switch')
+    btn.click();
+}
+
+function stopFlash(){
+    clearInterval(torchInterval)
+}
+
+
